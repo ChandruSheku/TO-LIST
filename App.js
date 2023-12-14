f606b3344da7d1111801b9a6f7661088051197ab
ChandruSheku <150648802+ChandruSheku@users.noreply.github.com>
2023-12-14 20:41:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
 //import logo from './logo.svg';
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './Components/Home';
-// import NavBar from './Components/NavBar';
-// import AboutUs from './Components/About';
-// import OrderSummary from './Components/OrderSummary';
-// import React from 'react';
-// import Counter from './Counter';
 import React from 'react';
 import Counter from './ENTRIPROJECT/Counter';
 const App = () => {
